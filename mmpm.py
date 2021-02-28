2b5ad0cb46cd3aaba81a9a07ba9acad198606bba
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-02-28 15:08:28 -0500

    Add commands

--- mmpm.py
+++ mmpm.py
@@ -1,4 +1,4 @@
-import sys
+import sys, requests, json
 class MMPMCommand:
   def __init__(self, function, arguments):
     self.arguments = arguments
@@ -8,8 +8,33 @@ class MMPMCommand:
       print(f'Not enough arguments, expected {self.arguments}, got {len(args)}')
       exit(1)
     self.callback(*args)
+def sync(package):
+  package = requests.get(f'https://www.graffbt.com/mmpm/{package}.json')
+  if package.status != 200:
+    print(f'Failed to get package, status code {package.status}')
+    exit(1)
+  filename = f'{package}.json'
+  file = open(filename, 'w')
+  file.write(package.body)
+def update():
+  db = requests.get(f'https://www.graffbt.com/mmpm/all.json')
+  if db.status != 200:
+    print(f'Failed to get database, status code {package.status}')
+    exit(1)
+  for i in db.json()['urls']:
+    print(f'Get {i}')
+    package = requests.get(f'https://www.graffbt.com/mmpm/{package}.json')
+    if package.status != 200:
+        print(f'Miss {package.status}')
+    else:
+      print('Hit')
+      filename = f'{package}.json'
+      file = open(filename, 'w')
+      file.write(package.body)
     
-commands = {}
+    
+  
+commands = {'sync':MMPMCommand(sync, 1), 'update':MMPMCommand(update, 0)}
 def help():
   print('MMPM Commands')
   print('mmpm sync <package>: Gets a package\'s metadata from MMPI')
